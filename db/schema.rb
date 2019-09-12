7b20c3155c9b73a3a5661c0a042f7ead7b3f65af
Maxence POINTUD <maxence.pointud@gmail.com>
2019-09-12 16:05:46 +0200

    up seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_11_124752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "lesson_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_appointments_on_lesson_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "join_table_user_swimming_pools", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "swimming_pool_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["swimming_pool_id"], name: "index_join_table_user_swimming_pools_on_swimming_pool_id"
+    t.index ["user_id"], name: "index_join_table_user_swimming_pools_on_user_id"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.datetime "start_date"
+    t.integer "duration"
+    t.string "title"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "swimming_pools", force: :cascade do |t|
+    t.integer "name"
+    t.string "adress"
+    t.string "city"
+    t.integer "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Lesson.destroy_all
+User.destroy_all
+SwimmingPool.destroy_all
+Appointment.destroy_all
+JoinTableUserSwimmingPool.destroy_all
+
+
+10.times do  |i|
+	title = ['Aquagym', 'Aquabike', 'Aquarunning', 'Aquabody', 'Aquapunching', 'Cours de natation', 'Bébés nageurs']
+	description = "Voici notre fabuleux cours de #{title}"
+	price = rand(1..1000)
+	Lesson.create(title: title.sample, description: description, price: price)
+end
+puts "10 lesson create"
+
+10.times do |i|
+	name = Faker::JapaneseMedia::DragonBall.character
+	adress = Faker::Address.street_address
+	zipcode = Faker::Address.zip_code
+	SwimmingPool.create(name: name, adress: adress, zipcode: zipcode)
+end
+puts "10 swimming_pool create"
+
+10.times do |i|
+	first_name = Faker::Games::Pokemon.name
+	last_name = Faker::Games::Pokemon.name
+	email = Faker::Internet.email
+	password = "coucou"
+	User.create(first_name: first_name, last_name: last_name, email: email, password: password)
+end
+puts "10 user create"
+
+10.times do |i|
+	user = User.all.sample
+	lesson = Lesson.all.sample
+	Appointment.create(user: user, lesson: lesson)
+end
+puts "10 Appointment create"
+
+10.times do |i|
+	user = User.all.sample
+	swimming_pool = SwimmingPool.all.sample
+	JoinTableUserSwimmingPool.create(user: user, swimming_pool: swimming_pool)
+end
+puts "10 JoinTableUserSwimmingPool createrails"
+
+
+
