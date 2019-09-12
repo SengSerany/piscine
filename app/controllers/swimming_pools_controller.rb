d805605e6b05fa2fc51300ea0a639f838eec0d8c
SengSerany <cranyseng@gmail.com>
2019-09-12 17:34:34 +0200

    add_form_for_add_pools

--- app/controllers/swimming_pools_controller.rb
+++ app/controllers/swimming_pools_controller.rb
@@ -1,11 +1,15 @@
 class SwimmingPoolsController < ApplicationController
+before_action :set_swimming_pool, only: [:show, :edit, :update, :destroy]
+
   def index
+    @swimming_pools = SwimmingPool.all
   end
 
   def show
   end
 
   def new
+    @swimming_pool = SwimmingPool.new
   end
 
   def create
@@ -19,4 +23,11 @@ class SwimmingPoolsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def set_swimming_pool
+    @swimming_pool = SwimmingPools.find(params[:id])
+  end
+
 end
